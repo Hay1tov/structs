e2a735f8814258b1e35d54ecccbdac0e3cdc9d8e
Hay1tov <eldorh35@gmail.com>
2024-12-14 14:51:55 +0500

    Bajarildi

---
+++ users.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+struct User{
+    char first_name[100];
+    char last_name[100];
+    int birth_year;
+    char location[100];
+    char phone[100];
+
+};
+int main(){
+
+    int total;
+    printf("Nechta foydalanuvchi ma'lumoti kerak: "); scanf("%d", &total);
+
+    
+    struct User users[total];
+    for(int i = 0; i < total; i++){
+
+        printf("%d- Foydalanuvchi \n\n%d. First name: ",i + 1,i+1);
+        scanf("%s", users[i].first_name);
+
+        printf("%d. Last name: ", i+1);
+        scanf("%s", users[i].last_name);
+
+        printf("%d. Tug'ilgan yil: ", i+1);
+        scanf("%d", &users[i].birth_year);
+
+        printf("%d. Manzil: ", i+1);
+        scanf("%s", users[i].location);
+
+        printf("%d. Telefon raqam: ", i+1);
+        scanf("%s", users[i].phone);
+    }
+    int count = users[total].birth_year;
+    printf("Foydalanuvchilar jadvali:\n First Name | Last Name | Birth Year | Location | Phone");
+
+    for (int i = 0; i < total; i++){
+
+        if (count <= users[i].birth_year )
+        {
+            printf("%15s", users[i].first_name);
+
+            printf("| %10s", users[i].last_name);
+
+            printf("| %8d", users[i].birth_year);
+
+            printf("| %s", users[i].location);
+
+            printf("| %s", users[i].phone);
+        }   
+    }
+    return 0;
+}
